ce4941db2479171caf34927a80cf6db5d62f59d7
zidon <jiangzedong@newborn-town.com>
2016-12-08 17:32:04 +0800

    tj

--- Swift/iOS10SwiftCoreData/iOS10SwiftCoreData/ViewController.swift
+++ Swift/iOS10SwiftCoreData/iOS10SwiftCoreData/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
