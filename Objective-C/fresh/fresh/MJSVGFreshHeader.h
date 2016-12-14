6bd5488490e98e7c7ac35c12ffb85dd676016b17
zidon <jiangzedong@newborn-town.com>
2016-12-14 13:44:55 +0800

    mjFresh添加svg刷新动画

---
+++ Objective-C/fresh/fresh/MJSVGFreshHeader.h
@@ -0,0 +1,15 @@
+//
+//  MJSVGFreshHeader.h
+//  fresh
+//
+//  Created by zidonj on 2016/12/14.
+//  Copyright © 2016年 zidon. All rights reserved.
+//
+
+#import "MJRefreshStateHeader.h"
+
+@interface MJSVGFreshHeader : MJRefreshStateHeader
+
+-(void)configSvgHeader;
+
+@end
